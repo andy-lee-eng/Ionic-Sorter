a23519c4457d2d27b95e0c8089fa43f5e4ec96b3
Andrew Lee <alee@scottlogic.co.uk>
2014-11-14 10:44:32 +0000

    Initial version of sorter project

---
+++ www/js/controllers.js
@@ -0,0 +1,21 @@
+angular.module('starter.controllers', [])
+
+.controller('HomeCtrl', function ($scope) {
+    $scope.contacts = [
+        { name: 'Frank', img: 'frank.jpg', phone: '0101 123456', mobile: '0770 123456', email: 'frank@emailionicsorter.com' },
+        { name: 'Susan', img: 'susan.jpg', phone: '0101 123456', mobile: '0770 123456', email: 'frank@emailionicsorter.com' },
+        { name: 'Emma', img: 'emma.jpg', phone: '0101 123456', mobile: '0770 123456', email: 'frank@emailionicsorter.com' },
+        { name: 'Scott', img: 'scott.jpg', phone: '0101 123456', mobile: '0770 123456', email: 'frank@emailionicsorter.com' },
+        { name: 'Bob', img: 'bob.jpg', phone: '0101 123456', mobile: '0770 123456', email: 'frank@emailionicsorter.com' },
+        { name: 'Olivia', img: 'olivia.jpg', phone: '0101 123456', mobile: '0770 123456', email: 'frank@emailionicsorter.com' },
+        { name: 'Anne', img: 'anne.jpg', phone: '0101 123456', mobile: '0770 123456', email: 'frank@emailionicsorter.com' }
+    ];
+
+    $scope.showReorder = false;
+    $scope.toggleReorder = function () { $scope.showReorder = !$scope.showReorder; };
+
+    $scope.onReorder = function (fromIndex, toIndex) {
+        var moved = $scope.contacts.splice(fromIndex, 1);
+        $scope.contacts.splice(toIndex, 0, moved[0]);
+    };
+});
